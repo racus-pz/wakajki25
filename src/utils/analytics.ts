6d56ed8425eb18a16b6325afa4d0e3dbdca4f858
racus-pz <75995491+racus-pz@users.noreply.github.com>
2025-06-11 17:13:40 +0200

    Update analytics.ts
    And GA

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -50,6 +50,15 @@ export const initializeAnalytics = (gaTrackingId: string) => {
       x?.parentNode?.insertBefore(s, x);
     })(window, document, 'script');
   }
+
+  //GA
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PWHZTCDT');</script>
+  <!-- End Google Tag Manager -->
 };
 
 export const trackPageView = (path: string) => {
